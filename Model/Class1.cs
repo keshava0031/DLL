1e0128d4fb194e3f01c2704d3d73c1880642b7c7
admin <admin@DESKTOP-IS34MOK>
2021-03-16 23:23:36 +0530

    Add project files.

---
+++ Model/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Model
+{
+    public class UserActions
+    {
+        public void Mail(string receiver, string subject, string message)
+        {
+            var senderEmail = new MailAddress("razorpay0031@gmail.com", "Admin");
+            var receiverEmail = new MailAddress(receiver, "Receiver");
+            var password = "kanini@123";
+            var sub = subject;
+            var body = message;
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = true,
+                Credentials = new NetworkCredential(senderEmail.Address, password)
+            };
+            using (var mess = new MailMessage(senderEmail, receiverEmail)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(mess);
+            }
+        }
+    }
+}
---
+++ Model/Ngo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Ngo
+    {
+        public string NId { get; set; }
+        public string NgName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string LoginAs { get; set; }
+    }
+}
